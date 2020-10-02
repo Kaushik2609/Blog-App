9ddf8b0948dac78dbf5c303156a956dc695b017f
Kaushik2609 <kaushikshinde26@gmail.com>
2020-10-02 19:06:21 +0530

    commit

---
+++ setup/config.js
@@ -0,0 +1,6 @@
+const key = {
+    url: 'mongodb+srv://Kaushik265:pwNVwz1pnzPLh8Im@cluster0.f3b6w.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    secret: "mysecret"
+}
+
+module.exports = key;
