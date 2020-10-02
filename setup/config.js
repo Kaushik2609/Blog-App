caf3c949d1eae96b1c412c70b1fe7145cbd81623
Kaushik2609 <kaushikshinde26@gmail.com>
2020-10-02 19:09:02 +0530

    commit

--- setup/config.js
+++ setup/config.js
@@ -1,5 +1,5 @@
 const key = {
-    url: 'mongodb+srv://Kaushik265:pwNVwz1pnzPLh8Im@cluster0.f3b6w.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    url: 'mongodb+srv://username:password@cluster0.f3b6w.mongodb.net/<dbname>?retryWrites=true&w=majority',
     secret: "mysecret"
 }
 
